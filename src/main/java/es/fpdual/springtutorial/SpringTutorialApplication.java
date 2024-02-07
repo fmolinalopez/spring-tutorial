a57875d8555bc8ca924344ac31a721a1d392039c
AYTOS\francisco.molina <francisco.molina@berger-levrault.com>
2024-02-07 18:24:41 +0100

    Base para trabajar con thymeleaf

--- src/main/java/es/fpdual/springtutorial/SpringTutorialApplication.java
+++ src/main/java/es/fpdual/springtutorial/SpringTutorialApplication.java
@@ -8,13 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class SpringTutorialApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ac = SpringApplication.run(SpringTutorialApplication.class, args);
-		
-		Prueba prueba = ac.getBean(Prueba.class);
-		Prueba prueba2 = ac.getBean(Prueba.class);
-		
-		System.out.println(prueba);
-		System.out.println(prueba2);
+		SpringApplication.run(SpringTutorialApplication.class, args);
 	}
-
 }
